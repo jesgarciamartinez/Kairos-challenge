2d97827f8f4b1b5a73aaee6fafb3ee3d0eff0012
Jesús García Martínez <jesus.garcia.martinez.contractor@bbva.com>
2016-04-08 04:24:35 +0200

    styling, routing

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,81 +1,63 @@
-/*
-Copyright (c) 2015 The Polymer Project Authors. All rights reserved.
-This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
-The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
-The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
-Code distributed by Google as part of the polymer project is also
-subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
-*/
-
-(function(document) {
+(function (document) {
   'use strict';
 
-  // Grab a reference to our auto-binding template
-  // and give it some initial binding values
-  // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
   var app = document.querySelector('#app');
 
-  // Sets app default base URL
   app.baseUrl = '/';
-  if (window.location.port === '') {  // if production
-    // Uncomment app.baseURL below and
-    // set app.baseURL to '/your-pathname/' if running from folder in production
-    // app.baseUrl = '/polymer-starter-kit/';
+  if (window.location.port === '') { // if production
+    app.baseUrl = '/kairos-challenge/';
   }
 
-  app.displayInstalledToast = function() {
-    // Check to make sure caching is actually enabled—it won't be in the dev environment.
-    if (!Polymer.dom(document).querySelector('platinum-sw-cache').disabled) {
-      Polymer.dom(document).querySelector('#caching-complete').show();
+  app.selected = 0;
+  app.entryAnimation = 'slide-from-right-animation';
+  app.exitAnimation = 'slide-left-animation';
+
+  app._prev = function () {
+    this.entryAnimation = 'slide-from-left-animation';
+    this.exitAnimation = 'slide-right-animation';
+    this.selected = this.selected === 0 ? 0 : (this.selected - 1);
+  };
+  app._next = function () {
+    this.entryAnimation = 'slide-from-right-animation';
+    this.exitAnimation = 'slide-left-animation';
+    var last = app.slides.length - 1;
+    this.selected = this.selected === last ? last : (this.selected + 1);
+  };
+  app._track = function (e) {
+    e.preventDefault();
+    var started = e.detail.state === 'start';
+    var goRight = e.detail.dx < 0;
+    if (started !== undefined) {
+      console.log('started ', started);
+      if (goRight) {
+        this._next();
+      } else {
+        this._prev();
+      }
     }
   };
 
-  // Listen for template bound event to know when bindings
-  // have resolved and content has been stamped to the page
-  app.addEventListener('dom-change', function() {
-    console.log('Our app is ready to rock!');
+  var body = document.querySelector('body');
+  body.addEventListener('my-dots-selected-changed', function (e) {
+    console.log('listener for selected ', e);
+    var values = e.detail;
+    var goRight = values.newValue > values.oldValue;
+    app.entryAnimation = goRight ?
+      'slide-from-right-animation' :
+      'slide-from-left-animation';
+    app.exitAnimation = goRight ?
+      'slide-left-animation' :
+      'slide-right-animation';
+    app.selected = values.newValue;
   });
 
-  // See https://github.com/Polymer/polymer/issues/1381
-  window.addEventListener('WebComponentsReady', function() {
-    // imports are loaded and elements have been registered
+  window.addEventListener('WebComponentsReady', function () {
+    var slider = document.querySelector('#slider');
+    var mutationObserver = new MutationObserver(mutationObserverCallback);
+    mutationObserver.observe(slider, {childList: true });
   });
 
-  // Main area's paper-scroll-header-panel custom condensing transformation of
-  // the appName in the middle-container and the bottom title in the bottom-container.
-  // The appName is moved to top and shrunk on condensing. The bottom sub title
-  // is shrunk to nothing on condensing.
-  window.addEventListener('paper-header-transform', function(e) {
-    var appName = Polymer.dom(document).querySelector('#mainToolbar .app-name');
-    var middleContainer = Polymer.dom(document).querySelector('#mainToolbar .middle-container');
-    var bottomContainer = Polymer.dom(document).querySelector('#mainToolbar .bottom-container');
-    var detail = e.detail;
-    var heightDiff = detail.height - detail.condensedHeight;
-    var yRatio = Math.min(1, detail.y / heightDiff);
-    // appName max size when condensed. The smaller the number the smaller the condensed size.
-    var maxMiddleScale = 0.50;
-    var auxHeight = heightDiff - detail.y;
-    var auxScale = heightDiff / (1 - maxMiddleScale);
-    var scaleMiddle = Math.max(maxMiddleScale, auxHeight / auxScale + maxMiddleScale);
-    var scaleBottom = 1 - yRatio;
-
-    // Move/translate middleContainer
-    Polymer.Base.transform('translate3d(0,' + yRatio * 100 + '%,0)', middleContainer);
-
-    // Scale bottomContainer and bottom sub title to nothing and back
-    Polymer.Base.transform('scale(' + scaleBottom + ') translateZ(0)', bottomContainer);
-
-    // Scale middleContainer appName
-    Polymer.Base.transform('scale(' + scaleMiddle + ') translateZ(0)', appName);
-  });
-
-  // Scroll page to top and expand header
-  app.scrollPageToTop = function() {
-    app.$.headerPanelMain.scrollToTop(true);
+  var mutationObserverCallback = function () {
+    document.querySelector('#spinner').removeAttribute('active');
   };
-
-  app.closeDrawer = function() {
-    app.$.paperDrawerPanel.closeDrawer();
-  };
-
 })(document);
