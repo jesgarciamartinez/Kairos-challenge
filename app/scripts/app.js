9c723a440e11978568e707ff7d4c6ae299ac57bb
Jesús García Martínez <jesus.garcia.martinez.contractor@bbva.com>
2016-04-11 02:30:21 +0200

    feat: dynamically make wide images 100% width, tall images 100% height

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -28,7 +28,6 @@
     var started = e.detail.state === 'start';
     var goRight = e.detail.dx < 0;
     if (started !== undefined) {
-      console.log('started ', started, goRight);
       if (goRight) {
         this._next();
       } else {
@@ -39,7 +38,6 @@
 
   var body = document.querySelector('body');
   body.addEventListener('my-dots-selected-changed', function (e) {
-    console.log('listener for selected ', e);
     var values = e.detail;
     var goRight = values.newValue > values.oldValue;
     app.entryAnimation = goRight ?
@@ -64,8 +62,31 @@
       .removeAttribute('active');
 
     // fix pages having no height
-    var sliderHeight = document.querySelector('#slider').getBoundingClientRect().height;
-    document.querySelector('#pages').style.height = sliderHeight + 'px';
+    var sliderRect = document.querySelector('#slider').getBoundingClientRect();
+    var pages = document.querySelector('#pages');
+    pages.style.height = sliderRect.height + 'px';
+
+    // make wide images 100% width, tall images 100% height
+
+    var imgs = [].slice.call(document.querySelectorAll('#pages img'));
+    imgs.forEach(function (img) {
+      if (img.naturalWidth >= img.naturalHeight) {
+        img.style.width = '100%';
+        img.style.height = 'auto';
+
+        var imgHeight = pages.getBoundingClientRect().width * img.naturalHeight / img.naturalWidth;
+        var imgPos = sliderRect.height / 2 - imgHeight / 2;
+        console.log(imgPos);
+        img.style.top = imgPos + 'px';
+      } else {
+        img.style.height = '100%';
+        img.style.width = 'auto';
+
+        var imgWidth = sliderRect.height * img.naturalWidth / img.naturalHeight;
+        var imgPos = pages.width / 2 - imgWidth / 2;
+        img.style.left = imgPos + 'px';
+      }
+    });
   };
 
   app.year = new Date()
